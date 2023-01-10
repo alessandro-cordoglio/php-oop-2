ab12ed24a01f3265c2a5ebb053a3107aa5ef3a9c
alessandro-cordoglio <alessandro.cord20@gmail.com>
2023-01-10 21:30:43 +0100

    test nuovo Gioco

--- index.php
+++ index.php
@@ -5,5 +5,9 @@ require_once __DIR__ . "/classes/Gioco.php";
 require_once __DIR__ . "/classes/Cuccia.php";
 require_once __DIR__ . "/classes/Cibo.php";
 
-$prodotto1= new Prodotto("test","test2","test3",new Categoria("cane"));
+$prodotto1= new Gioco(200,"dummy1","dummy2");
+$prodotto1->detailProdotto1="dettagli1";
+$prodotto1->detailProdotto2="dettagli2";
+$prodotto1->detailProdotto3="dettagli3";
+$prodotto1->categoria=new Categoria("cane");
 var_dump($prodotto1);
