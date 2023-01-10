15878a48bbd043da81de2b51c45dcdd7abbe4f1e
alessandro-cordoglio <alessandro.cord20@gmail.com>
2023-01-10 20:49:20 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+require_once __DIR__ . "/classes/Prodotto.php";
+require_once __DIR__ . "/classes/Categoria.php";
+require_once __DIR__ . "/classes/Gioco.php";
+require_once __DIR__ . "/classes/Cuccia.php";
+require_once __DIR__ . "/classes/Cibo.php";
